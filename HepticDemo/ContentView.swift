11effb1e460d66aab41afe4fcf7e6112a378ba81
sheikhabayzid <sheikhbayazid@gmail.com>
2020-08-10 12:39:55 +0600

    Initial Commit

---
+++ HepticDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HepticDemo
+//
+//  Created by Sheikh Bayazid on 8/10/20.
+//  Copyright © 2020 Sheikh Bayazid. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
